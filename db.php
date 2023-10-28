5ac91488153d89b54de1bf4dc7cce65f6dcae2dc
Paraskhilosiya <paraskhilosiyapkofficial@gmail.com>
2023-10-28 19:14:54 +0530

    server name updated

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect('localhost','root','','user');
+$con = mysqli_connect('database-1.ckl8ejoe8bnd.us-east-1.rds.amazonaws.com','admin123','admin123','sample');
 if($con){
 	
 }else{
