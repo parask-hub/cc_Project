484af0c0a1f66506bb2cc9e64e68de0556fbc727
Paraskhilosiya <paraskhilosiyapkofficial@gmail.com>
2023-10-31 13:33:38 +0530

    Updated finally

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
-// $con = mysqli_connect('database-1.ckl8ejoe8bnd.us-east-1.rds.amazonaws.com','admin123','admin123','sample');
-$con = mysqli_connect('localhost','root','','user');
+$con = mysqli_connect('database-1.ckl8ejoe8bnd.us-east-1.rds.amazonaws.com','admin123','admin123','sample');
+// $con = mysqli_connect('localhost','root','','user');
 if($con){
 	
 }else{
